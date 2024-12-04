d73f3a823a6b8fe71c6e027736c7e72a470bee93
JP <jplmarotinho22@gmail.com>
2024-12-04 12:04:54 -0300

    AddressRelationManager created, AddressRelationManager included into OrderResource

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Filament\Resources\OrderResource\RelationManagers\AddressRelationManager;
 use App\Models\Order;
 use App\Models\Product;
 use Filament\Actions\ActionGroup;
@@ -247,7 +248,7 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            AddressRelationManager::class
         ];
     }
 

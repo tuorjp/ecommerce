64cbc5b3e03602621969d6f341afa08ae276e670
JP <jplmarotinho22@gmail.com>
2024-12-04 18:02:35 -0300

    blades design inserted, basic front-end routes created

---
+++ app/Livewire/Auth/Login/Page.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Auth\Login;
+
+use Livewire\Component;
+
+class Page extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.login.page');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,35 @@
 <?php
 
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\Login\Page;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\CancelPage;
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
+use Filament\Pages\Auth\PasswordReset\ResetPassword;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);
+
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+
+Route::get('/login', Page::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forgot', ForgotPasswordPage::class);
+Route::get('/reset', ResetPassword::class);
+
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);

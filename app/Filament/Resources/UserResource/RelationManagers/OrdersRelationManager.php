0fc269ad7c8030559ff28e9de62ad24308acde3a
JP <jplmarotinho22@gmail.com>
2024-12-04 15:07:20 -0300

    Relationship configuration: User hasMany Order created, OrdersRelationManager created and included in UserResource

---
+++ app/Filament/Resources/UserResource/RelationManagers/OrdersRelationManager.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use App\Filament\Resources\OrderResource;
+use App\Models\Order;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use function PHPUnit\Framework\matches;
+
+class OrdersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'orders';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('id')
+            ->columns([
+                TextColumn::make('id')
+                    ->label('Order ID')
+                    ->sortable(),
+                TextColumn::make('grand_total')
+                    ->money('BRL'),
+                TextColumn::make('status')
+                    ->badge()
+                    ->color(fn(string $state):string => match($state){
+                        'new' => 'info',
+                        'processing' => 'warning',
+                        'shipped' => 'success',
+                        'delivered' => 'success',
+                        'canceled' => 'canceled',
+                    })
+                    ->icon(fn(string $state):string => match($state){
+                        'new' => 'heroicon-m-sparkles',
+                        'processing' => 'heroicon-m-arrow-path',
+                        'shipped' => 'heroicon-m-truck',
+                        'delivered' => 'heroicon-m-home',
+                        'canceled' => 'heroicon-m-x-circle',
+                    })
+                    ->sortable(),
+
+                TextColumn::make('payment_method')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('payment-status')
+                    ->sortable()
+                    ->searchable()
+                    ->badge(),
+                
+                TextColumn::make('created_at')
+                    ->label('Order Date')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                // Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Action::make('View Order')
+                    ->url(fn(Order $record):string => OrderResource::getUrl('view', ['record' => $record]))
+                    ->color('info')
+                    ->icon('heroicon-o-eye'),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}

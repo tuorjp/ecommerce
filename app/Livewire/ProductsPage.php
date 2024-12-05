a4003c8070dbd4eb41bd6a6abd1a58e2ad4028bf
tuorjp <jplmarotinho22@gmail.com>
2024-12-05 11:24:08 -0300

    products-page dynamic data, product pagination implemented

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('Products - Ecommerce project')]
 class ProductsPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.products-page');
+        $productsQuery = Product::query()->where('is_active', 1);
+        return view('livewire.products-page', [
+            'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
+            'products' => $productsQuery->paginate(6),
+        ]);
     }
 }

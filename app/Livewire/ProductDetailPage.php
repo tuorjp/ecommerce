ab288d8b6a23bb8143dcc07dc60bd9ac77d34c17
tuorjp <jplmarotinho22@gmail.com>
2024-12-05 11:42:00 -0300

    product-detail-page dynamic data added

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Product Detail - Ecommerce Project')]
 class ProductDetailPage extends Component
 {
+    public $slug;
+
+    //passa o slug da url para o atributo slug da classe
+    public function mount($slug) {
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail(),
+        ]);
     }
 }
